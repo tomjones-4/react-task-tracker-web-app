d0a22bf6768e49b0c548f0c260ae32b67a9c5847
tomjones-4 <thomasdjones4@gmail.com>
2025-03-23 12:53:18 -1000

    Add component skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import TodoList from "./TodoList.jsx";
+import TodoList from "./components/TodoList.jsx";
+import AddTodo from "./components/AddTodo.jsx";
 
 function App() {
   return (
     <div className="App">
       <TodoList />
+      <AddTodo />
     </div>
   );
 }
---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddTodo = () => {
+  return (
+    <div>
+      <button class="add-button">Add Button</button>
+    </div>
+  );
+};
+
+export default AddTodo;
